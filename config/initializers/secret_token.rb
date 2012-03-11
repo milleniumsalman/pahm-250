a827661c2db097b443bd5830e36dfc8593e281c5
Salman Ahmed <milleniumsalman@gmail.com>
2012-03-11 18:42:32 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pahm250::Application.config.secret_token = '77c53fda158f7eca44926682631e2a69bbb009160e7dfe8bac72bca52f15102f732e7268f37d970a57c3d02e31bd80f0bdbe3a4eb81537388fe4f7944b6e77e7'
